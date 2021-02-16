eab8a0f3916bfa7ba530868fbdf831e485fcb98f
Yosri-ctrl <yosribouabid@gmail.com>
2021-02-16 19:21:45 +0100

    task 1-13

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,20 @@
+function countPrimeNumbers() {
+	for (let i = 2; i <= 100; i++) {
+		var prime = true;
+		for (let j = 2; j < i; j++) {
+			if (j % i == 0) {
+				prime = false;
+			}
+		}
+		if (prime == true) {
+			console.log(i);
+		}
+	}
+}
+
+const t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+const t1 = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`);
---
+++ 0x0C-Javascript_advanced/13-bind_user.js
@@ -0,0 +1,14 @@
+var user = {
+	hobby: "Calligraphy",
+	favoriteSport: "Hockey",
+	astrologicalSign: "Aries",
+	firstName: "Buillaume",
+	lastName: "Ialva",
+	location: "Telaviv",
+	occupation: "Engineer",
+}
+function logWelcomeUser(welcomeString) {
+	console.log(welcomeString + ", " + user.firstName + ". Your occupation is: " + user.occupation)
+}
+let bindLogWelcomeUser = logWelcomeUser.bind(user);
+logWelcomeUser("Welcome");
